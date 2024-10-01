b19f84abad436fa4b49022e702d9fb3af136fb22
VinJust <vpj97@outlook.de>
2024-10-01 14:41:17 +0200

    Übungsaufgabe 2 in aufgabe2.py gelöst.

---
+++ aufgabe2.py
@@ -0,0 +1,19 @@
+# Variablen
+a = 10
+b = 3
+
+# Berechnungen
+summe = a + b
+differenz = a - b
+produkt = a * b
+quotient = a // b
+rest = a % b
+potenz = a ** b
+
+# Ausgabe
+print("Summe:", summe)
+print("Differenz:", differenz)
+print("Produkt:", produkt)
+print("Quotient (ganzzahlig):", quotient)
+print("Rest:", rest)
+print("Potenz:", potenz)
