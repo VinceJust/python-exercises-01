0e888b6968798f7b1fa17f6ac2aa360d75cbee68
VinJust <vpj97@outlook.de>
2024-10-01 14:28:43 +0200

    Übungsaufgabe 1 in aufgabe1.py gelöst.

---
+++ aufgabe1.py
@@ -0,0 +1,11 @@
+# Definieren von Variablen
+first_name = "Vincent"
+last_name = "Justkowski"
+birth_year = 1997
+has_license = False
+
+# Ausgabe auf der Konsole
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Hat Führerschein:", has_license)
