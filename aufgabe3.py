081559fafbf198ec7666e5f340f214a934c5fb4b
VinJust <vpj97@outlook.de>
2024-10-01 14:46:28 +0200

    Übungsaufgabe 3 in aufgabe3.py gelöst.

---
+++ aufgabe3.py
@@ -0,0 +1,8 @@
+# Funktion zur Umrechnung von Celsius in Fahrenheit
+def celsius_to_fahrenheit(celsius):
+  fahrenheit = (9/5) * celsius + 32
+  return fahrenheit
+
+# Funktion aufrufen
+print(celsius_to_fahrenheit(25))
+print(celsius_to_fahrenheit(10))
